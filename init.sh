f98c2d5f0bf5c91fccdc845ebd8511210abbd384
arri-cc <arri@arri.io>
2021-02-10 20:54:00 -0500

    wip

--- init.sh
+++ init.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-gcloud container clusters create gke-auditd --zone us-east1-b --num-nodes 1 --cluster-version 1.17.14-gke.1600 --machine-type g1-small --image-type COS_CONTAINERD --quiet 
+gcloud beta container clusters create gke-auditd \
+    --zone us-east1-b \
+    --num-nodes 3 \
+    --enable-logging-monitoring-system-only \
+    --scopes=logging-write \
+    --cluster-version 1.17.14-gke.1600 \
+    --machine-type g1-small \
+    --image-type COS_CONTAINERD \
+    --quiet 
